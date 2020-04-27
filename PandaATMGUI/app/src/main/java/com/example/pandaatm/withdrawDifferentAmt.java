c1288ae580fe60b9b9570a0cd57dd00acd099c72
Ashley Yu <ashley182018@gmail.com>
2020-04-26 23:40:32 -0700

    updated button functionalities and variable names

---
+++ PandaATMGUI/app/src/main/java/com/example/pandaatm/withdrawDifferentAmt.java
@@ -0,0 +1,15 @@
+package com.example.pandaatm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class withdrawDifferentAmt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw_diffamt_screen);
+    }
+
+}
